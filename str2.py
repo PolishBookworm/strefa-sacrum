6774092d88c81fe8c5ed5eec54840f19533304d5
SmugglerZ <160791722+SmugglerZ@users.noreply.github.com>
2025-02-01 16:28:59 +0100

    basic_podstrony

---
+++ str2.py
@@ -0,0 +1,55 @@
+import kivy
+kivy.require('2.3.1')
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty, ListProperty
+from kivy.lang import Builder
+from kivy import metrics
+from kivy.metrics import dp
+from kivy.core.window import Window
+
+Builder.load_file('str2.kv')
+
+class ScreenZwei(Screen):
+
+    menu_wl = BooleanProperty()
+    menu_wi = NumericProperty()
+
+    wysokosc_opcji = StringProperty()
+    szerokosc_opcji = StringProperty()
+    wysokosc_menu = NumericProperty()
+
+    kolor_tekstu = ListProperty()
+    kolor_tla = ListProperty()
+    kolor_akcentu = ListProperty()
+
+
+    def karta(self, k):
+        app = App.get_running_app()
+        screen_manager = app.root
+        screen_manager.current = 'str%d' %k
+
+    def wlacznik_menu(self, widget):
+        if widget.state == "normal":
+            self.menu_wl = True
+            self.menu_wi = 0
+        else:
+            self.menu_wl = False
+            self.menu_wi = 1
+
+    def on_size(self, *args):
+        lajout = self.ids.lajout
+        opcie = self.ids.opcie
+        pudlo = self.ids.pudlo
+
+        wys = Window.height - dp(48)
+        y_position = self.height - dp(self.wysokosc_menu)
+
+        opcie.height = wys
+        lajout.height = wys
+        pudlo.pos = (0, y_position)
